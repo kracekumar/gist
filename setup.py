19bc387e4d758344bafca0e273c25949ae0b77df
kracekumar <kracethekingmaker@gmail.com>
2012-07-20 00:23:58 +0530

    added include_package_data=True

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ from setuptools import setup, find_packages
 readme = open('README.rst', 'r').read()
 setup(
     name='gist',
-    version='0.0.7',
+    version='0.0.8b',
     url='https://github.com/kracekumar/gist',
     license='BSD',
     author='kracekumar',
     author_email='me@kracekumar.com',
     description='Command Line Interface for pasting to gist.github.com',
+    include_package_data=True,
     long_description=readme,
     packages=find_packages(),
     install_requires=['requests', 'argparse'],
